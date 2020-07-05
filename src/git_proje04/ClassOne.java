e6465209766cb5fdb6e22d43ad8d984b910494b0
suadahoxha <67202442+suadahoxha@users.noreply.github.com>
2020-07-05 14:49:54 -0400

    Update ClassOne.java
    ilk commit remote feature branch tan

--- src/git_proje04/ClassOne.java
+++ src/git_proje04/ClassOne.java
@@ -4,6 +4,7 @@ public class ClassOne {
 
 	public static void main(String[] args) {
 		System.out.println("ilk commit local Masterdan");
+		System.out.println("ilk commit local Masterdan");
 	}
 
 }
